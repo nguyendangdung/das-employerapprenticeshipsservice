cda8ce91d35f91a127ec05b9de5668739bc59d6d
RAJESHVELUC560B\rajesh-grace <rajesh@digi2al.co.uk>
2017-11-17 14:12:46 +0000

    Floating header implementation to release branch

--- src/SFA.DAS.EAS.Web/dist/javascripts/app.js
+++ src/SFA.DAS.EAS.Web/dist/javascripts/app.js
@@ -366,6 +366,16 @@ if (errorMessage.length > 0) {
     }
 }
 
-
+//floating header script
+$(window).scroll(function () {
+    if ($(window).scrollTop() >= 110) {
+        $('.floating-menu').addClass('fixed-header');
+        $('.js-float').addClass('width-adjust');
+    }
+    else {
+        $('.floating-menu').removeClass('fixed-header');
+        $('.js-float').removeClass('width-adjust');
+    }
+});
 
 
