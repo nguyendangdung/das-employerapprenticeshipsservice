7b4d069fb2f4e04e5e6bd423b0ce1545c5fdc5cc
DScuffle <david.scuffle@accomplish-it.co.uk>
2018-03-26 12:58:36 +0100

    Re-worked GetAllTransactionDetailsForAccountByDate Stored Procs, selects payments in a different way

--- src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAllTransactionDetailsForAccountByDate.sql
+++ src/SFA.DAS.EAS.Employer_Financial.Database/StoredProcedures/GetAllTransactionDetailsForAccountByDate.sql
@@ -56,6 +56,59 @@ AND DateCreated >= @FromDate
 AND DateCreated < @ToDate
 
 
+
+-- We will use this View to get payments that relate to transaction lines and are funding source 1,2,3
+;WITH AllFunds (DateCreated, AccountId, Ukprn, Uln, PeriodEnd, PaymentId, FundingSource, Amount, PaymentMetaDataId, TransactionTypeDesc,
+	TrainingProvider,
+	Apprentice,
+	ApprenticeTrainingCourse,
+	ApprenticeTrainingCourseLevel)
+AS
+(
+--	SELECT PaymentId, FundingSource, Amount, p.AccountId, p.Ukprn, p.Uln, p.PeriodEnd, p.PaymentMetaDataId
+	SELECT 
+			transLine.DateCreated,
+			p.AccountId, 
+			p.Ukprn, 
+			p.Uln, 
+			p.PeriodEnd, 
+			PaymentId, 
+			FundingSource, 
+			Amount, 
+			p.PaymentMetaDataId, 
+			ptt.[Description] AS TransactionTypeDesc,
+			meta.ProviderName AS TrainingProvider,
+			meta.ApprenticeName AS Apprentice,
+			meta.ApprenticeshipCourseName AS ApprenticeTrainingCourse,
+			meta.ApprenticeshipCourseLevel AS ApprenticeTrainingCourseLevel
+	from [employer_financial].[Payment]   p
+		INNER JOIN (SELECT PeriodEnd,AccountId,ukprn, EmpRef, TransactionDate, DateCreated, LevyDeclared, EnglishFraction 
+					FROM employer_financial.TransactionLine 
+					WHERE DateCreated >= @FromDate AND 
+						DateCreated <= @ToDate) transLine 
+			ON transLine.AccountId = p.AccountId 
+				AND transLine.PeriodEnd = p.PeriodEnd 
+				AND transLine.Ukprn = p.Ukprn
+		INNER JOIN [employer_financial].[PaymentTransactionTypes] ptt 
+			ON ptt.TransactionType =  p.TransactionType
+		INNER JOIN [employer_financial].[PaymentMetaData] meta 
+			ON p.PaymentMetaDataId = meta.Id
+	WHERE  p.AccountId = @AccountId
+		AND DateCreated >= @FromDate 
+		AND DateCreated <= @ToDate
+		AND p.FundingSource in (1,2,3)
+)
+, UniqueApprenticePaymentRecords -- This gets all the unique Account, Provider, Learner, PeriodEnd combinations
+AS
+(
+SELECT AccountId, Ukprn, Uln, PeriodEnd 
+	FROM [employer_financial].[Payment]   p
+		WHERE  p.AccountId = @AccountId
+			AND p.FundingSource in (1,2,3)
+	GROUP BY AccountId, Ukprn, Uln, PeriodEnd
+)
+
+
 INSERT INTO #output
 (
 	DateCreated,
@@ -70,51 +123,45 @@ INSERT INTO #output
 	GovermentContribution,
 	Total
 )
-SELECT MAX(transLine.DateCreated) AS DateCreated,
-	ptt.[Description],
-	meta.ProviderName AS TrainingProvider,
-	p.Uln AS Uln,
-	meta.ApprenticeName AS Apprentice,
-	meta.ApprenticeshipCourseName AS ApprenticeTrainingCourse,
-	meta.ApprenticeshipCourseLevel AS ApprenticeTrainingCourseLevel,
-	COALESCE((SUM(pays1.[Amount]) * -1), 0) AS PaidFromLevy,
-	COALESCE((SUM(pays3.Amount) * -1), 0) AS EmployerContribution,
-	COALESCE((SUM(pays2.Amount) * -1), 0) AS GovermentContribution,
-	COALESCE((SUM(pays1.[Amount]) * -1),0) + COALESCE((SUM(pays2.[Amount]) * -1),0) + COALESCE((SUM(pays3.[Amount]) * -1),0) AS Total
 
-FROM [employer_financial].[Payment] p
-  INNER JOIN [employer_financial].[PaymentTransactionTypes] ptt ON ptt.TransactionType =  p.TransactionType
-  INNER JOIN [employer_financial].[PaymentMetaData] meta ON p.PaymentMetaDataId = meta.Id
-  INNER JOIN (SELECT PeriodEnd,AccountId,ukprn, EmpRef, TransactionDate, DateCreated, LevyDeclared, EnglishFraction FROM employer_financial.TransactionLine WHERE DateCreated >= @FromDate AND 
-        DateCreated <= @ToDate) transLine ON transLine.AccountId = p.AccountId AND transLine.PeriodEnd = p.PeriodEnd AND transLine.Ukprn = p.Ukprn
-  LEFT JOIN [employer_financial].[Payment] pays1 
-	ON pays1.AccountId = p.AccountId 
-		AND pays1.Ukprn = p.Ukprn 
-		AND pays1.FundingSource = 1 
-		AND pays1.PaymentMetaDataId = meta.id
-  LEFT JOIN [employer_financial].[Payment] pays2 
-	ON pays2.AccountId = p.AccountId 
-	AND pays2.Ukprn = p.Ukprn 
-	AND pays2.FundingSource = 2 
-	AND pays2.PaymentMetaDataId = meta.id
-  LEFT JOIN [employer_financial].[Payment] pays3 
-	ON pays3.AccountId = p.AccountId 
-	AND pays3.Ukprn = p.Ukprn 
-	AND pays3.FundingSource = 3 
-	AND pays3.PaymentMetaDataId = meta.id
-  WHERE 
-	  p.AccountId = @accountid AND
-	  p.FundingSource IN (1,2,3)  
-  GROUP BY p.AccountId, 
-	  p.Ukprn, 
-	  p.TransactionType, 
-	  ptt.[Description],
-	  p.Uln, 
-	  meta.ProviderName, 
-	  meta.ApprenticeshipCourseName, 
-	  meta.ApprenticeshipCourseLevel, 
-	  meta.PathwayName, 
-	  meta.ApprenticeName
+-- Join the Views together
+SELECT DISTINCT 
+	COALESCE(MAX(accountFunds1.DateCreated),MAX(accountFunds2.DateCreated),MAX(accountFunds3.DateCreated)) AS DateCreated,
+	COALESCE(MAX(accountFunds1.TransactionTypeDesc),MAX(accountFunds2.TransactionTypeDesc),MAX(accountFunds3.TransactionTypeDesc)) AS [Description],
+	COALESCE(MAX(accountFunds1.TrainingProvider),MAX(accountFunds2.TrainingProvider),MAX(accountFunds3.TrainingProvider)) AS TrainingProvider,
+	uniquePayments.Uln AS Uln, 
+	COALESCE(MAX(accountFunds1.Apprentice),MAX(accountFunds2.Apprentice),MAX(accountFunds3.Apprentice)) AS Apprentice,
+	COALESCE(MAX(accountFunds1.ApprenticeTrainingCourse), MAX(accountFunds2.ApprenticeTrainingCourse), MAX(accountFunds3.ApprenticeTrainingCourse)) AS ApprenticeTrainingCourse,
+	COALESCE(MAX(accountFunds1.ApprenticeTrainingCourseLevel), MAX(accountFunds2.ApprenticeTrainingCourseLevel), MAX(accountFunds3.ApprenticeTrainingCourseLevel)) AS ApprenticeTrainingCourseLevel,
+	COALESCE((SUM(accountFunds1.[Amount]) * -1), 0) AS PaidFromLevy,
+	COALESCE((SUM(accountFunds3.Amount) * -1), 0) AS EmployerContribution,
+	COALESCE((SUM(accountFunds2.Amount) * -1), 0) AS GovermentContribution,
+	COALESCE((SUM(accountFunds1.[Amount]) * -1),0) + COALESCE((SUM(accountFunds2.[Amount]) * -1),0) + COALESCE((SUM(accountFunds3.[Amount]) * -1),0) AS Total
+FROM UniqueApprenticePaymentRecords uniquePayments
+LEFT JOIN AllFunds accountFunds1 
+	ON accountFunds1.AccountId = uniquePayments.AccountId 
+		AND accountFunds1.Ukprn = uniquePayments.Ukprn 
+		AND accountFunds1.Uln = uniquePayments.Uln
+		AND accountFunds1.PeriodEnd = uniquePayments.PeriodEnd
+		AND accountFunds1.FundingSource = 1
+LEFT JOIN AllFunds accountFunds2 
+	ON accountFunds2.AccountId = uniquePayments.AccountId 
+		AND accountFunds2.Ukprn = uniquePayments.Ukprn 
+		AND accountFunds2.Uln = uniquePayments.Uln
+		AND accountFunds2.PeriodEnd = uniquePayments.PeriodEnd
+		AND accountFunds2.FundingSource = 2
+LEFT JOIN AllFunds accountFunds3
+	ON accountFunds3.AccountId = uniquePayments.AccountId 
+		AND accountFunds3.Ukprn = uniquePayments.Ukprn 
+		AND accountFunds3.Uln = uniquePayments.Uln
+		AND accountFunds3.PeriodEnd = uniquePayments.PeriodEnd
+		AND accountFunds3.FundingSource = 3
+GROUP BY uniquePayments.AccountId, 
+	uniquePayments.Ukprn, 
+	uniquePayments.Uln, 
+	uniquePayments.PeriodEnd
+HAVING
+	MAX(accountFunds1.[Amount]) > 0 OR MAX(accountFunds2.[Amount]) > 0 OR MAX(accountFunds3.[Amount]) > 0
 
 SELECT * FROM #output
 
